1176c22799886bb7745d22bf35bf6e0629171e32
obickford <bickfoor@miamioh.edu>
2022-04-19 20:35:54 -0400

    worked on it some, have some basic functions that all mesh togehter, close but not still cant get the hist or the sample for 10,000 lengths, confused

--- homework 4 workspace.R
+++ homework 4 workspace.R
@@ -1,6 +1,6 @@
 
 
-directions <- function(num) {
+directions <- function(num) { ##basic collatz sequence 
   if(num == 1) {
     return(NULL)
   }
@@ -13,43 +13,56 @@ directions <- function(num) {
   }
 }
 
-stated_hw_problem <- function(n) {
+collatzloop_trial <- function(n) { ## Prints out the collatz sequence for a given n value 
   stated_problem <- n
   while (n != 1) {
-    n <- directions(n)
+    n <- directions(n=n)
     stated_problem <- c(stated_problem, n)
   }
   return(stated_problem)
 }
+collatzloop_trial(n=3)
 
-stated_hw_problem(6)
 
 
-collatz_length <- function(n) { ##without the looping over 1:10000
-  stated_problem <- n
-    while (n != 1) {
-    n <- directions(n)
-    stated_problem <- c(stated_problem, n)
+###########################################
+## Combination of the above functions, adding in vectors
+
+CollatzLoop1 <- function(x,n){ ##This i think loops over 1-10000, i get the lengths 
+  for(x in 1:n){
+    collatz_vector <- c()
+    for(i in 1:10000){
+      if (x == 1){
+        collatz_vector <- c(collatz_vector, x)
+        break
+      } else {
+        if (x %% 2 == 0){
+          collatz_vector <- c(collatz_vector, x)
+          x <- x/2
+        } else {
+          collatz_vector <- c(collatz_vector, x)
+          x <- 3*x+1
+        }
+      }
     }
-  length(stated_problem)
-}
-collatz_length(6)
-
-#######################################
-col_length <- c()
-  for(i in 1:10000) {
-    col_length <- c(col_length,
-                    collatz_length)
-    length(col_length)
   }
-
-hist(col_length)
-
-#######################################
-
+  length(collatz_vector)
+}
+CollatzLoop1(n=3)
 
 
 
+###################################################
+## Trial
 
+collatzlength <- function(n) { ## Also gives sequence length 
+  collatz_length2 <- c()
+  for(i in 1:10000) {
+    collatz_length2 <- c(collatz_length2,
+                         CollatzLoop1(n=n))
+  }
+  mean(collatz_length2)
+}
+collatzlength(n=3)
 
 
